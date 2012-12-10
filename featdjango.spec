b6a08a0d36ebb406d9219be2e22b12237bdb3092
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-12-10 17:52:55 +0100

    package

---
+++ featdjango.spec
@@ -0,0 +1,58 @@
+%global __python python
+
+%{!?python_sitelib: %define python_sitelib %(%{__python} -c "from distutils.sysconfig import get_python_lib; print get_python_lib()")}
+%{!?pyver: %define pyver %(%{__python} -c "import sys ; print sys.version[:3]")}
+%define version 0.1.0
+
+Name:           python-featdjango
+Summary:        F3AT Django integration
+Version:        %{version}
+Release:        1%{?dist}
+Source0:        featdjango-%{version}.tar.gz
+
+Group:          Development/Languages
+License:        GPL
+URL:            http://www.flumotion.com
+
+BuildRoot:      %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+
+BuildRequires:  python-devel >= 2.6
+BuildRequires:  python-setuptools >= 0.6c9
+
+Requires:       python-twisted-core
+Requires:       python-twisted-web
+Requires:       nsca-client
+Requires:       python-feat
+
+Provides:       %{name}
+
+%description
+Flumotion Asynchronous Autonomous Agent Toolkit and Django integration.
+
+%prep
+%setup -q -n featdjango-%{version}
+
+%build
+CFLAGS="$RPM_OPT_FLAGS" %{__python} setup.py build
+
+%install
+rm -rf $RPM_BUILD_ROOT
+%{__python} setup.py install --skip-build --root=$RPM_BUILD_ROOT \
+     --record=INSTALLED_FILES
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+
+%files
+%defattr(-,root,root,-)
+
+%doc README.rst
+
+%{python_sitelib}/*
+
+
+%changelog
+* Mon Dec 10 2012 Thomas Vander Stichele <thomas at apestaart dot org>
+- 0.1.0-1
+- first version
---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# See "LICENSE.GPL" in the source distribution for more information.
+
+# Headers in this file shall remain intact.
+from setuptools import setup, find_packages
+
+
+NAME = 'featdjango'
+
+VERSION = '0.1.0'
+DESCRIPTION = 'F3AT and Django integration'
+LONG_DESC = DESCRIPTION
+AUTHOR = 'Marek Kowalski',
+AUTHOR_EMAIL = 'mkowalski@empaytech.com',
+URL = 'https://github.com/mkowalski/featdjango'
+LICENSE = "GPL"
+PLATFORMS = ['any']
+REQUIRES = ['twisted', 'twisted.web', 'json', 'feat']
+SETUP_REQUIRES = ['setuptools>=0.6c9']
+INSTALL_REQUIRES = ['Twisted>=10.1']
+KEYWORDS = ['twisted', 'agent', 'framework']
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: No Input/Output (Daemon)',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Natural Language :: English',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python :: 2.6',
+    'Topic :: Software Development :: Libraries :: Application Frameworks']
+
+
+setup(name = NAME,
+      version = VERSION,
+      description = DESCRIPTION,
+      long_description = LONG_DESC,
+      author = AUTHOR,
+      author_email = AUTHOR_EMAIL,
+      url = URL,
+      license = LICENSE,
+      platforms = PLATFORMS,
+      setup_requires = SETUP_REQUIRES,
+      install_requires = INSTALL_REQUIRES,
+      requires = REQUIRES,
+      package_dir = {'': 'src'},
+      packages = find_packages('src'),
+      include_package_data = True,
+      keywords = KEYWORDS,
+      classifiers = CLASSIFIERS)

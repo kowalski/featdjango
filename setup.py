36d4779a5cf2429022271e8b8af44b0aa1f2abde
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2014-04-25 20:30:04 -0400

    bump build

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 NAME = 'featdjango'
 
-VERSION = '0.3.0'
+VERSION = '0.3.1'
 DESCRIPTION = 'F3AT and Django integration'
 LONG_DESC = DESCRIPTION
 AUTHOR = 'Marek Kowalski',
